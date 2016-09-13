fba93a14644c849b0201b14489380d5ca1b33e6b
Oleg SKLYANCHUK <sklyanchuk@gmail.com>
2016-09-13 16:21:24 +0200

    refactor(webapp/filebox): simplify file/filelist decision

--- src/webapp/components/filebox/filebox.js
+++ src/webapp/components/filebox/filebox.js
@@ -2,11 +2,7 @@ import filelist from '../filelist/filelist';
 import file from '../file/file';
 
 export default function filebox(subtree) {
-  switch (subtree.type) {
-  case 'file':
-    return file(subtree);
-  case 'dir':
-  default:
-    return filelist(subtree);
-  }
+  return subtree.type === 'file' ?
+    file(subtree) :
+    filelist(subtree);
 }
